37f877d9ccd7b8995f5147ebe8be5271f55ee465
Steffen Staerz <steffen.staerz@cern.ch>
2020-12-01 19:43:47 -0500

    Migrate rx_fifo_module to avst_packet interface

--- simulation/ethernet_lib/rx_fifo_module/rx_fifo_module_tb.vhd
+++ simulation/ethernet_lib/rx_fifo_module/rx_fifo_module_tb.vhd
@@ -12,26 +12,26 @@
 -------------------------------------------------------------------------------
 
 --! @cond
-library IEEE;
-  use IEEE.std_logic_1164.all;
+library fpga;
+  context fpga.interfaces;
 --! @endcond
 
 --! Testbench for rx_fifo_module.vhd
 entity rx_fifo_module_tb is
   generic (
     --! File containing the reset input data
-    FIFO_DAT_FILENAME  : string := "sim_data_files/FIFO_data_in.dat";
+    AVST_RXD_FILE  : string := "sim_data_files/FIFO_data_in.dat";
     --! File to write out the response of the reset_module
-    FIFO_LOG_FILENAME  : string := "sim_data_files/FIFO_data_out.dat";
+    AVST_TXD_FILE  : string := "sim_data_files/FIFO_data_out.dat";
     --! File containing counters on which the rx interface is not ready
-    FIFO_RX_READY_FILE : string := "sim_data_files/FIFO_rx_ready_in.dat";
+    AVST_RDY_FILE  : string := "sim_data_files/FIFO_rx_ready_in.dat";
     --! File containing counters on which a manual reset is carried out
-    MNL_RST_FILE       : string := "sim_data_files/MNL_RST_in.dat";
+    MNL_RST_FILE   : string := "sim_data_files/MNL_RST_in.dat";
 
     --! Flag to use to indicate comments
-    COMMENT_FLAG       : character := '%';
+    COMMENT_FLAG   : character := '%';
     --! Flat to use to indicate counters
-    COUNTER_FLAG       : character := '@';
+    COUNTER_FLAG   : character := '@';
 
     --! Locking the FIFO on the writing side
     LOCK_FIFO      : boolean := true;
@@ -52,45 +52,37 @@ library ethernet_lib;
 architecture tb of rx_fifo_module_tb is
 
   --! Clock
-  signal clk              : std_logic;
+  signal clk           : std_logic;
   --! Reset, sync with #clk
-  signal rst              : std_logic;
+  signal rst           : std_logic;
 
-  --! @name Avalon-ST from input
-  --! @{
-
-  --! RX ready
-  signal fifo_rx_ready     : std_logic;
-  --! RX data
-  signal fifo_rx_data      : std_logic_vector(63 downto 0);
-  --! RX controls
-  signal fifo_rx_ctrl      : std_logic_vector(6 downto 0);
-
-  --! @}
-
-  --! @name Avalon-ST to output
+  --! @name Avalon-ST to module (read from file)
   --! @{
 
   --! TX ready
-  signal fifo_tx_ready     : std_logic;
-  --! TX data
-  signal fifo_tx_data      : std_logic_vector(63 downto 0);
-  --! TX controls
-  signal fifo_tx_ctrl      : std_logic_vector(6 downto 0);
+  signal tx_ready      : std_logic;
+  --! TX data and controls
+  signal tx_packet     : t_avst_packet(data(63 downto 0), empty(2 downto 0), error(0 downto 0));
 
   --! @}
 
-  --! @brief FIFO read empty
-  --! @todo Port could be removed as already indicated by status_vector(3)
-  signal rx_fifo_rd_empty  : std_logic;
+  --! @name Avalon-ST from module (written to file)
+  --! @{
+
+  --! RX ready
+  signal rx_ready      : std_logic;
+  --! RX data and controls
+  signal rx_packet     : t_avst_packet(data(63 downto 0), empty(2 downto 0), error(0 downto 0));
+
+  --! @}
 
   --! status of the module
-  signal status_vector  : std_logic_vector(4 downto 0);
+  signal status_vector : std_logic_vector(4 downto 0);
 
 begin
 
   --! Instantiate the Unit Under Test (UUT)
-  uut: entity ethernet_lib.rx_fifo_module
+  uut : entity ethernet_lib.rx_fifo_module
   generic map (
     LOCK_FIFO     => LOCK_FIFO,
     LOCK_FIFO_OUT => LOCK_FIFO_OUT,
@@ -98,31 +90,26 @@ begin
   )
   port map (
 
-    -- Reset, sync with rx_fifo_in_clk
-    rx_fifo_in_rst    => rst,
+    -- Reset, sync with clk_i
+    rst_i           => rst,
 
     --! Avalon-ST FIFO RX interface to load FIFO
-    rx_fifo_in_clk    => clk,
-    rx_fifo_in_ready  => fifo_tx_ready,
-    rx_fifo_in_data   => fifo_tx_data,
-    rx_fifo_in_ctrl   => fifo_tx_ctrl,
+    clk_i           => clk,
+    rx_ready_o      => tx_ready,
+    rx_packet_i     => tx_packet,
 
     --! Avalon-ST FIFO TX interface to empty FIFO
-    rx_fifo_out_clk   => clk,
-    rx_fifo_out_ready => fifo_rx_ready,
-    rx_fifo_out_data  => fifo_rx_data,
-    rx_fifo_out_ctrl  => fifo_rx_ctrl,
-
-    -- FIFO read empty
-    rx_fifo_rd_empty  => rx_fifo_rd_empty,
+    clk_o           => clk,
+    tx_ready_i      => rx_ready,
+    tx_packet_o     => rx_packet,
 
     -- Status of the module
-    status_vector     => status_vector
+    status_vector_o => status_vector
   );
 
   -- Simulation part
   -- generating stimuli based on counter
-  simulation: block
+  blk_simulation : block
     --! @cond
     signal counter    : integer := 0;
     signal async_rst  : std_logic;
@@ -160,7 +147,7 @@ begin
     async_rst <= sim_rst or mnl_rst;
 
     --! Instantiate delay_chain to generate rst
-    rst_sync_inst: entity misc.delay_chain
+    inst_rst_sync : entity misc.delay_chain
     port map (
       clk        => clk,
       rst        => '0',
@@ -168,74 +155,37 @@ begin
       sig_out(0) => rst
     );
 
-    fifo_tx_gen_block: block
-    begin
-      --! Instantiate av_st_sender to read rst_tx from FIFO_DAT_FILENAME
-      rst_rx_gen: entity sim.av_st_sender
-      generic map (
-        FILENAME      => FIFO_DAT_FILENAME,
-        COMMENT_FLAG  => COMMENT_FLAG,
-        COUNTER_FLAG  => COUNTER_FLAG
-      )
-      port map (
-        clk       => clk,
-        rst       => rst,
-        cnt       => counter,
+    --! Instantiate avst_packet_sender to read tx from AVST_RXD_FILE
+    inst_tx : entity ethernet_lib.avst_packet_sender
+    generic map (
+      FILENAME      => AVST_RXD_FILE,
+      COMMENT_FLAG  => COMMENT_FLAG,
+      COUNTER_FLAG  => COUNTER_FLAG
+    )
+    port map (
+      clk       => clk,
+      rst       => rst,
+      cnt       => counter,
 
-        -- Avalon-ST to outside world
-        tx_ready  => fifo_tx_ready,
-        tx_data   => fifo_tx_data,
-        tx_ctrl   => fifo_tx_ctrl
-      );
+      tx_ready  => tx_ready,
+      tx_packet => tx_packet
+    );
 
-    end block;
+    --! Instantiate avst_packet_receiver to write rx to AVST_TXD_FILE
+    inst_rx : entity ethernet_lib.avst_packet_receiver
+    generic map (
+      READY_FILE    => AVST_RDY_FILE,
+      DATA_FILE     => AVST_TXD_FILE,
+      COMMENT_FLAG  => COMMENT_FLAG
+    )
+    port map (
+      clk       => clk,
+      rst       => rst,
+      cnt       => counter,
 
-    fifo_log_gen: block
-      --! @cond
-      signal wren            : std_logic := '0';
-      signal fifo_rx_ready_n : std_logic := '0';
-      --! @endcond
-    begin
-
-      --! Instantiate counter_matcher to generate fifo_rx_ready_n
-      rx_ready_gen: entity sim.counter_matcher
-      generic map (
-        FILENAME      => FIFO_RX_READY_FILE,
-        COMMENT_FLAG  => COMMENT_FLAG
-      )
-      port map (
-        clk       => clk,
-        rst       => rst,
-        counter   => counter,
-        stimulus  => fifo_rx_ready_n
-      );
-
-      fifo_rx_ready <= not fifo_rx_ready_n;
-
-      -- logging block for rx interface
-      wren <= fifo_rx_ctrl(6) and fifo_rx_ready;
-
-      --! Instantiate file_writer_hex to write fifo_rx_data
-      log_rx: entity sim.file_writer_hex
-      generic map (
-        FILENAME      => FIFO_LOG_FILENAME,
-        COMMENT_FLAG  => COMMENT_FLAG,
-        BITSPERWORD   => 16,
-        WORDSPERLINE  => 4
-      )
-      port map (
-        clk       => clk,
-        rst       => rst,
-        wren      => wren,
-
-        empty     => fifo_rx_ctrl(2 downto 0),
-        eop       => fifo_rx_ctrl(4),
-        err       => fifo_rx_ctrl(3),
-
-        din       => fifo_rx_data
-      );
-
-    end block;
+      rx_ready  => rx_ready,
+      rx_packet => rx_packet
+    );
 
   end block;
 
