70dee8222050068a5d0dc2eb54857f91f386b7ec
Steffen Staerz <steffen.staerz@cern.ch>
2022-10-12 15:53:35 -0400

    Remove unused DHCP_TIMEOUT

--- simulation/xgbe_lib/dhcp_in_ip_module/dhcp_in_ip_module_tb.vhd
+++ simulation/xgbe_lib/dhcp_in_ip_module/dhcp_in_ip_module_tb.vhd
@@ -45,9 +45,7 @@ entity dhcp_in_ip_module_tb is
     MY_MAC         : std_logic_vector(47 downto 0) := x"00_22_8F_02_41_EE";
 
     --! Post-UDP-module UDP CRC calculation
-    UDP_CRC_EN     : boolean                 := false;
-    --! Timeout in milliseconds
-    DHCP_TIMEOUT   : integer range 2 to 1000 := 10;
+    UDP_CRC_EN     : boolean := false;
 
     --! Enable IP address filtering
     IP_FILTER_EN   : std_logic               := '1';
@@ -153,8 +151,7 @@ begin
   --! Instantiate the primary Unit Under Test (UUT): DHCP module
   uut1 : entity xgbe_lib.dhcp_module
   generic map (
-    UDP_CRC_EN   => UDP_CRC_EN,
-    DHCP_TIMEOUT => DHCP_TIMEOUT
+    UDP_CRC_EN => UDP_CRC_EN
   )
   port map (
     clk    => clk,
